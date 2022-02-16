f260b760e8e4c79e74c6621940d1135c3deaee4b
Miguel González <jmiguel@Mac-mini-de-Miguel.local>
2022-02-15 20:33:33 -0600

    Setup app component config

---
+++ src/utils/fonts-stack.ts
@@ -0,0 +1,8 @@
+const fonts = {
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+};
+
+export default fonts;
